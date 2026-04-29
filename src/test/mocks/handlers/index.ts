3343c739ff773928d016fd5596caa1abb66b71e5
LivingLikeKrillin <143606756+LivingLikeKrillin@users.noreply.github.com>
2026-04-29 11:59:33 +0900

    test(mocks): add report handlers + 7 fixtures (list/detail/orphan + 3 errors) (G2.3)

--- src/test/mocks/handlers/index.ts
+++ src/test/mocks/handlers/index.ts
@@ -1,5 +1,11 @@
 import { authHandlers } from "./auth"
 import { memberHandlers } from "./members"
 import { partyroomHandlers } from "./partyrooms"
+import { reportHandlers } from "./reports"
 
-export const handlers = [...authHandlers, ...memberHandlers, ...partyroomHandlers]
+export const handlers = [
+  ...authHandlers,
+  ...memberHandlers,
+  ...partyroomHandlers,
+  ...reportHandlers,
+]
